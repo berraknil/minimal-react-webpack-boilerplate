a6ebb2a4f666aa9a31b0bd44ab6edc6dada67bbd
Berrak Nil <berraknil@gmail.com>
2017-03-12 22:23:34 +0300

    New minimal boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './src/index.js'
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'react-hot-loader!babel-loader'
+    }]
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  }
+};
